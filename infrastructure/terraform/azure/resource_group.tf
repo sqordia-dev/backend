299afec8baff90ba2669956760c6997909789958
Faïçal Sawadogo <sqordia.dev@gmail.com>
2026-01-05 13:51:41 -0500

    Refactor Azure resource group configuration in Terraform by removing the import block and adding lifecycle rules to prevent destruction and ignore changes. This improves resource management and ensures stability for existing resources.

--- infrastructure/terraform/azure/resource_group.tf
+++ infrastructure/terraform/azure/resource_group.tf
@@ -1,10 +1,6 @@
-# Import existing resource group
-import {
-  to = azurerm_resource_group.main
-  id = "/subscriptions/83deee7a-77ea-4add-8c10-c93199c5ec54/resourceGroups/sqordia-production-rg"
-}
-
 # Resource Group
+# Note: This resource group already exists in Azure and is managed outside of Terraform creation
+# Using lifecycle rules to prevent recreation attempts
 resource "azurerm_resource_group" "main" {
   name     = "${var.project_name}-${var.environment}-rg"
   location = var.azure_location
@@ -15,5 +11,10 @@ resource "azurerm_resource_group" "main" {
       Name = "${var.project_name}-${var.environment}-rg"
     }
   )
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = all
+  }
 }
 
