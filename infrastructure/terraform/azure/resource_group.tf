5f814cf5a8a0e659c27475ff2739c9f8e0f33580
Faïçal Sawadogo <sqordia.dev@gmail.com>
2026-01-05 14:10:44 -0500

    Import existing Azure resource group into Terraform state and remove lifecycle rules to streamline resource management. This change enhances clarity in the configuration and ensures proper handling of existing resources.

--- infrastructure/terraform/azure/resource_group.tf
+++ infrastructure/terraform/azure/resource_group.tf
@@ -1,6 +1,10 @@
+# Import existing resource group into Terraform state
+import {
+  to = azurerm_resource_group.main
+  id = "/subscriptions/83deee7a-77ea-4add-8c10-c93199c5ec54/resourceGroups/sqordia-production-rg"
+}
+
 # Resource Group
-# Note: This resource group already exists in Azure and is managed outside of Terraform creation
-# Using lifecycle rules to prevent recreation attempts
 resource "azurerm_resource_group" "main" {
   name     = "${var.project_name}-${var.environment}-rg"
   location = var.azure_location
@@ -11,10 +15,5 @@ resource "azurerm_resource_group" "main" {
       Name = "${var.project_name}-${var.environment}-rg"
     }
   )
-
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes  = all
-  }
 }
 
